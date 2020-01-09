d9c132372f96d695829fe080b643bba1214eedd4
at-phuongle <phuong.le@asiantech.vn>
2020-01-09 14:07:26 +0700

    fix convention

--- JsExercise1/bt1.js
+++ JsExercise1/bt1.js
@@ -1,38 +1,38 @@
 //câu 1
 
 var num1, num2;
-function tinhTong(num1, num2) {
+function sum1(num1, num2) {
   if (num1 === num2) {
     return (num1 + num2) * 3;
   } else {
     return (num1 + num2);
   }
 }
-console.log("kq cau 1 la: " + tinhTong(2,2));
+console.log("result of exercise1: " + sum1(2,2));
 
 //câu 2
 var soNhapVao;
-function tinhHieu(soNhapVao) {
+function Subtraction(soNhapVao) {
   if (soNhapVao <= 19) {
     return (19 - soNhapVao);
   } else {
     return (soNhapVao - 19) * 3;
   }
 }
-console.log("kq cau 2 la: " + tinhHieu(9));
+console.log("result of exercise2: " + Subtraction(9));
 
 //câu 3
 var string = '';
 var number = [];
 var sum = 0;
-function soChiaHetCho3(string) {
+function Div3(string) {
   var arrDiv3 = [];
   number = string.split('');
   number.splice(string.indexOf('*'), 1);
-  for(var i = 0; i < number.length; i++) {
+  for (var i = 0; i < number.length; i++) {
     sum += parseInt(number[i]);
   }
-  for(var a = 0; a < 10; a++) {
+  for (var a = 0; a < 10; a++) {
     var t = sum;
     t += a;
     if (t % 3 === 0) {
@@ -41,17 +41,17 @@ function soChiaHetCho3(string) {
   }
   return arrDiv3;
 }
-console.log("kq cau 3 la: " + soChiaHetCho3('1*9'));
+console.log("result of exercise3: " + Div3('1234567890*'));
 
 //câu 4
 var arrDiv6 = [];
-function soChiaHetCho6(string) {
-  var abc = soChiaHetCho3(string);
-  for(var f = 0; f < abc.length; f++) {
+function Div6(string2) {
+  var abc = Div3(string2);
+  for (var f = 0; f < abc.length; f++) {
     if (abc[f] % 2 === 0) {
       arrDiv6.push(abc[f]);
     }
   }
   return arrDiv6;
 }
-console.log("kq cau 4 la: " + soChiaHetCho6('1234567890*'));
+console.log("result of exercise4: " + Div6('1234567890*'));
