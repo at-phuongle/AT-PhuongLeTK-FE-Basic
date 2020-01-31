a240a68a0a0d9ed09f029172590991347136f153
at-phuongle <phuong.le@asiantech.vn>
2020-01-31 14:54:23 +0700

    fix <td class=td-product-price> from cart.js

--- js-exercise-2/cart.js
+++ js-exercise-2/cart.js
@@ -12,7 +12,7 @@ function showCart() {
       '<td class="td-image"><img class="td-cart-img" src="./images/' + getData[i].object.image + '"></td>' +
       '<td class="td-product-name">' + getData[i].object.name + '</td>' +
       '<td class="td-product-quantity">' + getData[i].quantity + '</td>' +
-      '<td class"td-product-price">$' + getData[i].object.price + '</td>' +
+      '<td class="td-product-price">$' + getData[i].object.price + '</td>' +
       '<td class="td-product-sub-total">$' + (getData[i].object.price * getData[i].quantity) + '</td>' +
       //'<td class="td-product-delete" data-id="1" onclick="deleteItem(' + getData[i].object.id + ')"><i class="fa fa-times"></i></td>' +
       '<td class="td-product-delete"><button type="button" class="js-bt-delete" data-id="' + getData[i].object.id + '">X</button></td>' +
