f311e88a1cd1df16ce0759184aef46663765f70d
at-phuongle <phuong.le@asiantech.vn>
2020-02-17 14:24:23 +0700

    update module js

--- js-exercise-4/moduleStickyForList.js
+++ js-exercise-4/moduleStickyForList.js
@@ -1,10 +1,9 @@
-(function () {
-  var $boxs = document.getElementsByClassName("js-gr-pro");
-  var $titles = document.getElementsByClassName('js-gr-title');
-  var $list = document.getElementById('js-product-list');
-  $list.onscroll = function () { myFunction() };
+var $list = document.getElementById('js-product-list');
+var $boxs = document.getElementsByClassName("js-gr-pro");
+var $titles = document.getElementsByClassName('js-gr-title');
 
-  function myFunction() {
+var stickyHandle = (function () {
+  function ftSticky() {
     var minHeight = 0;
     var maxHeight = -25;
     var i = 0;
@@ -18,4 +17,8 @@
       i += 1;
     }
   }
-}());
+  return {
+    sticky: ftSticky
+  }
+})();
+$list.addEventListener('scroll', stickyHandle.sticky);
